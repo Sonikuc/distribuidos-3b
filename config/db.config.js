1d33065d4ff2b8394c34ac725ac13a1fae7ec3f7
Sonikuc <soniikkuc@hotmail.com>
2023-10-30 17:33:02 -0400

    Cambio para usar mongo local

--- config/db.config.js
+++ config/db.config.js
@@ -1,23 +1,18 @@
-const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
+const mongoose = require('mongoose');
+const uri = "mongodb+srv://admin:example@products.0zociic.mongodb.net/?retryWrites=true&w=majority";
+const connectDB = async () => {
+    try {
+        await mongoose.connect(uri,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            });
+        console.log("MongoDB Connected")
+    }
+    catch (error) {
+        console.error(error.message);
+        process.exit(1);
+    }
+}
 
-// uri from mongo atlas cloud db
-var uri = "mongodb+srv://suarezbrito:123@cluster0.al5wqgt.mongodb.net/?retryWrites=true&w=majority"
-
-// Declare a variable named option and assign optional settings
-const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-};
-
-// Connect MongoDB Atlas using mongoose connect method
-mongoose.connect(uri, options).then(() => {
-    console.log("Database connection established!");
-},
-    err => {
-        {
-            console.log("Error connecting Database instance due to:", err);
-        }
-    });
-
-module.exports = mongoose;
+module.exports = connectDB;
