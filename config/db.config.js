e9553ba4eab7953c41cc71563519fdb4c42455f9
Sonikuc <soniikkuc@hotmail.com>
2023-12-04 21:48:27 -0400

    update algo del db AYUDA

--- config/db.config.js
+++ config/db.config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
 // Variable de entorno con la conexión a la base de datos
-const uriEnv = 'mongodb://api-mongodb.pj-dasuarez-19.svc:27017/mongodb';
+const uriEnv = 'mongodb://api-mongodb.pj-dasuarez-19.svc.cluster.local:27017/mongodb';
 //const uriEnv = process.env.DB_NAME+'://'+process.env.APP_NAME+'-'+process.env.DB_NAME+'.'+process.env.PROJECT_NAME+'.svc:27017/'+process.env.DB_NAME;
 // Variable local que se usa para la conexión
 var uri = uriEnv;
